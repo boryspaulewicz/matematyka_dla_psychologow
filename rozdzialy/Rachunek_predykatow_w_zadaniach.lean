99d67f921a2ad06f6dd6daee309c68ebc0a06b31
Boryslaw Paulewicz <borys.paulewicz@gmail.com>
2025-07-08 00:15:21 +0200

    dodatki

--- rozdzialy/Rachunek_predykatow_w_zadaniach.lean
+++ rozdzialy/Rachunek_predykatow_w_zadaniach.lean
@@ -1,4 +1,5 @@
--- Proszę zgłaszać trudności, żebym wiedział, jak pomóc nie przeszkadzając za bardzo w nauce.
+-- Większość tych zadań "ukradłem" ze znakomitej książki "Theorem Proving in Lean 4". Proszę
+-- zgłaszać trudności, żebym wiedział, jak pomóc nie przeszkadzając za bardzo w nauce.
 
 -- O tym będzie mowa.
 variable {α : Type} {p q : α → Prop} {r : Prop}
@@ -6,7 +7,7 @@ variable {α : Type} {p q : α → Prop} {r : Prop}
 -- Warto tu użyć dopasowania wzorca. Term `x : α` nie odgrywa tu ważnej roli.
 example : (∃ x : α, r) → r := sorry
 
--- Tu musimy skorzystać z dostępnego termu `a` typu `α`.
+-- Tu musimy skorzystać z dostępnego termu `a` typu `α`, za to w "dziwny" sposób.
 example (a : α) : r → (∃ x : α, r) := sorry
 
 -- Dopasowanie wzorca (tutaj zagnieżdżone) jest Twoim przyjacielem!
@@ -16,13 +17,11 @@ example (a : α) : (∀ x : α, r) → r := sorry
 
 example : r → (∀ x : α, r) := sorry
 
--- To zadanie ilustruje "sens" zdania o postaci `∃ x, P x` i jednocześnie "sens" koproduktu.
+-- To zadanie ilustruje "sens" zdania o postaci `∃ x, P x`u.
 example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
 
--- Bo produkt jest operacją łączną i przemienną.
 example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := sorry
 
--- Produkt strzałek to strzałka między produktami (ale niekoniecznie odwrotnie).
 example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
 
 example : α → ((∀ x : α, r) ↔ r) := sorry
@@ -32,14 +31,15 @@ example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
 
 example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
 
--- Łączność i przemienność koproduktu.
 example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := sorry
 
--- Kwantyfikatory i negacja:
+-- Kwantyfikatory i negacja.
 example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
 
--- Reszta klasycznie (trzeba użyć `em`).
+-- Reszta klasycznie (trzeba użyć aksjomatu wyłączonego środka `em`, który znajdziemy w przestrzeni
+-- nazw `Classical`).
 section
+-- Dzięki temu do końca tej sekcji można pisać `em` zamiast `Classical.em`.
 open Classical
 
 example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
