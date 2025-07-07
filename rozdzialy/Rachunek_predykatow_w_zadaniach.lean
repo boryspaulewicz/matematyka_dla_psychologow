007dd99325ce7e7fd138d8bded4d26ee793ddfdd
Boryslaw Paulewicz <borys.paulewicz@gmail.com>
2025-07-08 00:28:53 +0200

    drobne

--- rozdzialy/Rachunek_predykatow_w_zadaniach.lean
+++ rozdzialy/Rachunek_predykatow_w_zadaniach.lean
@@ -17,7 +17,7 @@ example (a : α) : (∀ x : α, r) → r := sorry
 
 example : r → (∀ x : α, r) := sorry
 
--- To zadanie ilustruje "sens" zdania o postaci `∃ x, P x`u.
+-- To zadanie ilustruje "sens" zdania o postaci `∃ x, P x`.
 example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
 
 example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := sorry
@@ -33,7 +33,9 @@ example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
 
 example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := sorry
 
+----------------------------------------------------------------------------------------------------
 -- Kwantyfikatory i negacja.
+
 example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := sorry
 
 -- Reszta klasycznie (trzeba użyć aksjomatu wyłączonego środka `em`, który znajdziemy w przestrzeni
